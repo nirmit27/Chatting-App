42834a4c47fe3058e4dc0c7598f99141e012c01d
Nirmit Mishra <93557744+nirmit27@users.noreply.github.com>
2024-03-26 13:03:05 +0000

    socket.io integrated

--- index.js
+++ index.js
@@ -3,14 +3,23 @@
 const path= require('path')
 const express = require('express')
 const { createServer } = require('node:http')
+const { Server } = require('socket.io')
 
 const port = 3000
 
 const app = express()
 const server = createServer(app)
+const io = new Server(server)
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, "./index.html"))
+})
+
+io.on('connection', (socket)=>{
+    console.log("a user connected")
+    socket.on('disconnect', ()=>{
+        console.log("a user disconnected")
+    })
 })
 
 server.listen(port, ()=>{
