081b4d978649a8bdc8bcbc156440050004f50bdc
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-04-04 07:55:34 +0530

    ui changes

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -3,7 +3,7 @@ import Message from "./Message";
 export default function ChatContainer({ messages }) {
   return (
     <div
-      class="mt-4 py-3 px-4 mx-auto w-4/5 md:w-2/5 overflow-hidden rounded bg-slate-100 shadow-md"
+      class="py-3 px-2 mx-auto w-4/5 md:w-2/5 overflow-hidden rounded bg-slate-50 shadow-md"
       id="messages"
     >
       {messages.length ? (
